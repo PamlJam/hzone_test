2dc1fae145bcabb9dab248f031c1c4c6badc4a57
hjx <1606297247@qq.com>
2021-02-15 21:07:38 +0800

    一些前端美化，异步响应——登录判断

--- hzone/user/urls.py
+++ hzone/user/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
 urlpatterns =[
-    path('',views.room),
+    path('<int:user_id>',views.room),
+    path('update/',views.update),
+    # 信息编辑页面
     path('upload/',views.upload),
-    path('upload_icon/',views.upload_icon),
+    # 提交按钮
+    path('log_out/',views.log_out),
+    # 注销按钮
+    path('log_in/',views.log_in),
+    # 登录按钮
+
 ]
--- hzone/user/views.py
+++ hzone/user/views.py
@@ -1,24 +1,68 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-def upload_icon(request):
-    if request.method == 'POST':
-        icon = request.FILES.get('icon', None)
-        # 读取表单文件信息
-        if icon:
-            request.user.icon = icon
-            # 更改
-            request.user.save()
-            # 保存
-            return HttpResponse('seccess')
-        else:
-            return HttpResponse('error')
+from django.shortcuts import get_object_or_404
+from .models import User
+from django.contrib.auth import login
+from django.contrib.auth import logout
+from django.http import Http404
+from django.shortcuts import redirect
+from django.contrib.auth import authenticate
+from django.http import JsonResponse
 
-def room(request):
+def log_in(request):
+    
+    if request.user.is_authenticated:
+        raise Http404('None')
+    elif request.method == 'POST':
+        un = request.POST.get('un','')
+        pw = request.POST.get('pw','')
+        user = authenticate(request, username=un, password=pw)
+        context = {}
+        if user is not None:
+            context['status'] = 'SUCCESS'
+            login(request, user)
+        else:
+            context['status'] = 'ERROR'
+        return JsonResponse(context)
+    else:
+        return render(request,'login.html',{})
+
+def log_out(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect('/')
+    else:
+        raise Http404('None')
+
+def upload(request):
+    if request.method == 'POST':
+        user = request.user
+        icon = request.FILES.get('icon')
+        # 读取表单文件信息
+        motto = request.POST.get('motto')
+
+        email = request.POST.get('email')
+
+        if icon:
+            user.icon = icon
+        if motto:
+            user.motto = motto
+        if email:
+            user.email = email
+        user.save()
+
+        return render(request,'update.html',{})
+
+
+def room(request,user_id):
+    id_user = get_object_or_404(User,id = user_id)
     context = {}
+    context['id_user'] = id_user
+    
     response = render(request,'room.html',context)
     return response
 
-def upload(request):
+
+def update(request):
     context = {}
-    response = render(request,'upload.html',context)
+    response = render(request,'update.html',context)
     return response
