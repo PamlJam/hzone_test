ce864e7c4fc7791074b6a4aea0014647b68109e7
hjx <1606297247@qq.com>
2021-02-19 14:53:57 +0800

    博客列表采用了跳转至上次浏览位置的功能

--- hzone/blog/views.py
+++ hzone/blog/views.py
@@ -1,27 +1,22 @@
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from .models import Article
-from .models import ArticleType
-
 
 def blog_list(requset):
-    articles = Article.objects.all()
-    context = {}
-    context ['articles'] = articles
-
-    response = render(requset,'list.html',context)
-    
-    return response
-
+    if requset.method == 'GET':
+        articles = Article.objects.all()
+        context = {}
+        context ['articles'] = articles
+        context ['pos'] = requset.session.get('pos',default =  '0')
+        # 取出位置记录 默认值为0
+        requset.session['pos'] = requset.GET.get('pos','0')
+        # 记录实时位置
+        response = render(requset,'list.html',context)
+        return response
 
 def blog_detail(requset,article_pk):
-
     article = get_object_or_404(Article,pk = article_pk)
-
     context = {}    
-
     context ['article'] = article
-
     response = render(requset,'detail.html',context)
-
     return response
--- hzone/user/models.py
+++ hzone/user/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
-    motto = models.TextField(max_length=24,default="这个人很懒的啦，什么也没写")
+    motto = models.TextField(max_length=24)
     # 个性签名
     icon = models.ImageField(default="./static/icon/default.jpg",upload_to='./static/icon/')
     # 用户头像
